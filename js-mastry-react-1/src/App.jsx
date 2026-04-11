650c293a18fcdef56848ac2432a2fbf8f2e51070
Piyush <plovanshi@nuvizz.com>
2026-04-11 13:59:04 +0530

    useState and useEffect hooks

--- js-mastry-react-1/src/App.jsx
+++ js-mastry-react-1/src/App.jsx
@@ -1,17 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import {useState, useEffect} from 'react'
 import './App.css'
 
 const App = () => {
-  return (<>
-        <Card title="One"/>
-        <Card title="Two"/>
-        <Card title="Three"/>
-      </>
-  )
+    return (<div className='card-container '>
+            <Card title="One"/>
+            <Card title="Two"/>
+            <Card title="Three"/>
+        </div>
+    )
 }
 
-const Card = ({title})=> {return <h1>{title}</h1>}
+const Card = ({title}) => {
+    const [isLiked, setIsLiked] = useState(false);
+    const [likedUnlikedCount, setLikedUnlikedCount] = useState(0);
+
+    useEffect(() => {
+        console.log(`${title} has been liked/unliked ${likedUnlikedCount} times`)
+    },[isLiked]);
+    useEffect(()=>{                     // will run only on the rendering of the component due to empty dependency array
+        console.log(`card title:${title} rendered`);
+    },[]);
+
+    return <div className='card'>
+        <h2>{title}</h2>
+        <button onClick={() => {
+            setIsLiked(!isLiked);
+
+            // setLikedUnlikedCount(1+likedUnlikedCount)
+            //   the above is not correct way to update the state, we should use the below approach passing the callback function
+            //   that accepts the current state and then updates it, React state updates are asynchronous and batched.
+            // In above approach the value of  likedUnlikedCount inside your function is not
+            //   guaranteed to be the latest when multiple updates happen quickly.
+            setLikedUnlikedCount((likedUnlikedCount) => {
+                return 1 + likedUnlikedCount
+            })
+
+        }}>{!isLiked ? 'Like' : 'Unlike'}</button>
+    </div>
+}
 export default App
